067aabb0116886c5cdbf2389d53f2ae74d88f211
Mateusz <matt.wozakowski@gmail.com>
2026-09-15 17:20:58 +0100

    RSCBC-308: Getting strated API docs still refer to 1.0.0-beta.1

--- sdk/couchbase/src/lib.rs
+++ sdk/couchbase/src/lib.rs
@@ -30,7 +30,7 @@
 //!
 //! ```toml
 //! [dependencies]
-//! couchbase = "1.0.0-beta.1"
+//! couchbase = "1"
 //! tokio = { version = "1", features = ["full"] }
 //! serde = { version = "1", features = ["derive"] }
 //! serde_json = "1"
